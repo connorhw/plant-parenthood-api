a7f2171bf9f80ee1bc32408a39ef93d7fcc672df
connorhw <connorwong93@gmail.com>
2021-01-05 19:30:47 -0800

    heroku instance is now working, changed client origin in congif file to vercel url but still need to make sure that was the correct url to put in client origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    //CLIENT_ORIGIN:
+    CLIENT_ORIGIN: 'https://plant-parenthood-app-pyistkv2r.now.sh/',
   }
