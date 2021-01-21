577985e16bac62d1be407e032c0ee04f65b5fd0e
connorhw <connorwong93@gmail.com>
2021-01-21 14:25:36 -0800

    updated url in app?

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://plant-parenthood-app-pyistkv2r.now.sh/',
+    CLIENT_ORIGIN: 'https://plant-parenthood-api.herokuapp.com/',
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/pp-plants',
   }
+
+  //'https://plant-parenthood-app-pyistkv2r.now.sh/'
